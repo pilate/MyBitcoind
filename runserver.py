989e456dd755b0354394cc289aae3f0ce88ba525
Paul Westin <pilate@pilate.es>
2013-12-21 16:32:11 -0800

    per-request rpc connections
    rescan after last imported address

--- runserver.py
+++ runserver.py
@@ -1,12 +1,12 @@
 from bitcoinrpc.authproxy import AuthServiceProxy
 from bitcoinrpc.authproxy import JSONRPCException
+from socket import timeout
 
 import bottle as b
 
 
 # Setup
 b.TEMPLATE_PATH.append("templates")
-access = AuthServiceProxy("http://Pilate:password@127.0.0.1:8332")
 
 # Static file routes
 @b.get('/static/css/<filename:re:.*\.css>')
@@ -18,15 +18,29 @@ def static_js(filename):
     return b.static_file(filename, root='static/js')
 
 # Utility
-def get_balance():
-    unspent = access.listunspent(0)
+def get_balance(rpc):
+    unspent = rpc.listunspent(0)
     balance = sum(map(lambda u: u["amount"], unspent))
     return balance
 
 def get_context():
+    message = ""
+    try:
+        rpc_connection = AuthServiceProxy("http://Pilate:password@127.0.0.1:8332", timeout=1)
+        rpc_connection.getinfo()
+    except timeout:
+        rpc_connection = None
+        message = "Bitcoind connection failed."
+
+    if rpc_connection:
+        balance = get_balance(rpc_connection)
+    else:
+        balance = "Error"
+
     return {
-        "balance": get_balance(),
-        "message": ""
+        "balance": balance,
+        "message": message,
+        "rpc": rpc_connection
     }  
 
 # Address related routes
@@ -38,22 +52,33 @@ def index():
 @b.get('/addresses/list/')
 def address_list():
     out_obj = get_context()
-    out_obj["addresses"] = access.getaddressesbyaccount("")
+    rpc = out_obj["rpc"]
+    addresses = []
+    if rpc:
+        addresses = rpc.getaddressesbyaccount("")
+    out_obj["addresses"] = addresses
     return b.template('address_list', out_obj)
 
 @b.get('/addresses/unspent/')
 def address_unspent():
     out_obj = get_context()
-    out_obj["unspent"] = access.listunspent(0)
+    rpc = out_obj["rpc"]
+    unspent = []
+    if rpc:
+        unspent = rpc.listunspent(0)
+    out_obj["unspent"] = unspent
     return b.template('address_unspent', out_obj)
 
 @b.get('/addresses/received/')
 def address_received():
     out_obj = get_context()
-    addresses = access.listreceivedbyaddress(0)
-    addresses = sorted(addresses, key=lambda k: k["amount"], reverse=True)
-    for address in addresses:
-        address["amount"] = '{0:.8f}'.format(address["amount"])
+    rpc = out_obj["rpc"]
+    addresses = []
+    if rpc:
+        addresses = rpc.listreceivedbyaddress(0)
+        addresses = sorted(addresses, key=lambda k: k["amount"], reverse=True)
+        for address in addresses:
+            address["amount"] = '{0:.8f}'.format(address["amount"])
     out_obj["addresses"] = addresses
     return b.template('address_received', out_obj)
 
@@ -66,18 +91,26 @@ def import_list():
 @b.post('/import/list/')
 def import_list():
     out_obj = get_context()
+    out_obj["privkeys"] = ""
+    rpc = out_obj["rpc"]
     form_addresses = b.request.forms.get('privkeys')
     split_addresses = form_addresses.split("\n");
     clean_list = map(str.strip, split_addresses)
-    added = 0
-    for key in clean_list:
-        try:
-            access.importprivkey(key, "", False)
-        except JSONRPCException:
-            continue
-        else:
-            added += 1
-    out_obj["message"] = "Imported {0} new keys.".format(added)
+    if rpc:
+        added = 0
+        for key in clean_list:
+            rescan = False
+            if key == clean_list[-1]:
+                rescan = True
+            try:
+                rpc.importprivkey(key, "", rescan)
+            except JSONRPCException:
+                continue
+            else:
+                added += 1
+        out_obj["message"] = "Imported {0} new keys. Rescan started.".format(added)
+    else:
+        out_obj["privkeys"] = form_addresses
     return b.template('import_list', out_obj)
 
 b.run(host='loathes.asia', port=8080, debug=True, reloader=True)
--- templates/import_list.tpl
+++ templates/import_list.tpl
@@ -16,7 +16,7 @@
 <form method="POST">
     <div class="row">
         <div class="large-12 columns">
-            <textarea class="import-list" name="privkeys"></textarea>
+            <textarea class="import-list" name="privkeys">{{ privkeys }}</textarea>
         </div>
         <div class="large-12 columns">
             <input type="submit" class="button" value="Submit">
